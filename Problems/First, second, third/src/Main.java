426c680e5242921c1b28fcc2ae7aea90c8a3e8d0
BSanandu88 <59107214+BSanandu88@users.noreply.github.com>
2020-07-30 11:29:09 +0530

    Initial commit

---
+++ Problems/First, second, third/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // put your code here
+        System.out.println("first");
+        System.out.println("second");
+        System.out.println("third");
+
+    }
+}
